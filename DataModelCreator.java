bcca8f05e8b47ae29e17d8a3687ce3434398b188
Maya Mudambi <mayamudambi@gmail.com>
2016-12-08 00:44:19 -0500

    Data Model creator class fully implemented; produces a text file with each semester's ratings of various classes correlated properly. TesterMain class tests its functionality.
    TO DO:
    Implement Mahout to create a neighborhood

--- DataModelCreator.java
+++ DataModelCreator.java
@@ -3,31 +3,134 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * Creates a text file that serves as a data model for the Mahout recommender engine.
+ * @author mayamudambi
+ */
 
 public class DataModelCreator {
 
 	String modelFileName;
 	
-	public DataModelCreator(String modelFile){
+	private double courseQuality;
+    private double instructorQuality;
+    private double difficulty;
+    private double amountLearned;
+    private double workRequired;
+    private double readingsValue;
+    private double communication;
+    private double instructorAccess;
+    private double stimulateInterest;
+	private double taQuality;
+    private double recommendMajor;
+    private double recommendNonMajor;
+	
+	public DataModelCreator(String modelFile, double courseQual, double instQual, double diff, double amLrnd, double workReq, 
+			double readVal, double comm, double instAcc, double stimInst, double TA, double recMaj, double recNonMaj){
+		
 		modelFileName = modelFile;
+    	courseQuality = courseQual;
+    	instructorQuality = instQual;
+    	difficulty = diff;
+    	amountLearned = amLrnd;
+    	workRequired = workReq;
+    	readingsValue = readVal;
+    	communication = comm;
+    	instructorAccess = instAcc;
+    	stimulateInterest = stimInst;
+    	taQuality = TA;
+    	recommendMajor = recMaj;
+    	recommendNonMajor = recNonMaj;
 	}
 	
 	
 	public void createDataFile(HashMap<String, ArrayList<Review>> semesterPreferences) throws FileNotFoundException{
-		PrintWriter writer = new PrintWriter("review_data_model.txt");
-
-		int userID = 0;
+		PrintWriter writer = new PrintWriter(modelFileName);
 		
 		for(String semester : semesterPreferences.keySet()){
 			for(Review review : semesterPreferences.get(semester)){
-				String dataLine = userID + "," + review.getCourse().getCourseCode() + "," + 
+				String dataLine = semester + "," + review.getCourse().getCourseCode() + "," + getReviewRating(review); 
+				writer.println(dataLine);
 			}
-			
 		}
 	    writer.close();
 	}
 	
-	private void generateRating(Review r){
-		double overallRating;
+	private double getReviewRating(Review r){
+		
+		int ratingCategories = generateSignificantCategories(r);
+		
+		double courseQualityWeight = (double)(courseQuality/5.0)*(r.getCourseQuality());
+	    double instructorQualityWeight = (double)(instructorQuality/5.0)*(r.getInstructorQuality());
+	    double difficultyWeight = (double)(difficulty/5.0)*(r.getDifficulty());
+	    double amountLearnedWeight = (double)(amountLearned/5.0)*(r.getAmountLearned()); 
+	    double workRequiredWeight = (double)(workRequired/5.0)*(r.getWorkRequired());
+	    double readingsValueWeight = (double)(readingsValue/5.0)*(r.getReadingsValue());
+	    double communicationWeight = (double)(communication/5.0)*(r.getCommunication());
+	    double instructorAccessWeight = (double)(instructorAccess/5.0)*(r.getInstructorAccess());
+	    double stimulateInterestWeight = (double)(stimulateInterest/5.0)*(r.getStimulateInterest());
+		double taQualityWeight = (double)(taQuality/5.0)*(r.getTaQuality());
+	    double recommendMajorWeight = (double)(recommendMajor/5.0)*(r.getRecommendMajor());
+	    double nonRecommendMajorWeight = (double)(recommendNonMajor/5.0)*(r.getRecommendNonMajor());
+		
+		double rating = (double)(courseQualityWeight + instructorQualityWeight + difficultyWeight + amountLearnedWeight + workRequiredWeight +
+				readingsValueWeight + communicationWeight + instructorAccessWeight + stimulateInterestWeight + taQualityWeight +
+				recommendMajorWeight + nonRecommendMajorWeight)/ratingCategories;
+		
+		return rating;
+	}
+	
+	private int generateSignificantCategories(Review r){
+		int ratingCategories = 0;
+		
+		if(r.getCourseQuality() != 0.0){
+			ratingCategories++;
+		}
+		
+		if(r.getInstructorQuality() != 0.0){
+			ratingCategories++;
+		}
+		
+		if(r.getDifficulty() != 0.0){
+			ratingCategories++;
+		}
+		
+		if(r.getAmountLearned() != 0.0){
+			ratingCategories++;
+		}
+		
+		if(r.getWorkRequired() != 0.0){
+			ratingCategories++;
+		}
+		
+		if(r.getReadingsValue() != 0.0){
+			ratingCategories++;
+		}
+		
+		if(r.getCommunication() != 0.0){
+			ratingCategories++;
+		}
+		
+		if(r.getInstructorAccess() != 0.0){
+			ratingCategories++;
+		}
+		
+		if(r.getStimulateInterest() != 0.0){
+			ratingCategories++;
+		}
+		
+		if(r.getTaQuality() != 0.0){
+			ratingCategories++;
+		}
+		
+		if(r.getRecommendMajor() != 0.0){
+			ratingCategories++;
+		}
+		
+		if(r.getRecommendNonMajor() != 0.0){
+			ratingCategories++;
+		}	
+		
+		return ratingCategories;
 	}
 }
--- testermain.java
+++ testermain.java
@@ -1,27 +1,53 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
 
 
 public class testermain {
 
 	/**
-	 * @param args
+	 * @param argsd
 	 */
 	public static void main(String[] args) {	
 		
 		try {
+			Scanner scan = new Scanner(System.in);
+			
 			DataProducer d = new DataProducer();
 			HashMap<String, ArrayList<Review>> h = new HashMap<String, ArrayList<Review>>();
 			
 			h = d.getData();
 			
-			for(String s : h.keySet()){
-				System.out.println(s);
-				for(Review r : h.get(s)){
-					System.out.println(r.getCourse().getCourseTitle());
-				}
-			}
+			System.out.println("For the following questions, please respond with any number between 0.0 and 5.0 to indicate how important this course attribute is to you.");
+			System.out.println("How important is the overall course quality?");
+			double courseQuality = scan.nextDouble();
+			System.out.println("How important is the overall instructor quality?");
+		    double instructorQuality = scan.nextDouble();
+			System.out.println("How important is it that the course is challenging?");
+		    double difficulty = scan.nextDouble();
+			System.out.println("How important is it that you learn a lot from this course?");
+		    double amountLearned = scan.nextDouble();
+			System.out.println("How important is it that you do a lot of work for this course?");
+		    double workRequired = scan.nextDouble();
+			System.out.println("How important is it that the course's readings are of a good quality?");
+		    double readingsValue = scan.nextDouble();
+			System.out.println("How important is the instructor's quality of communication?");
+		    double communication = scan.nextDouble();
+			System.out.println("How important is the instructor's level of availability?");
+		    double instructorAccess = scan.nextDouble();
+			System.out.println("How important is it that the course stimluates your interest?");
+		    double stimulateInterest = scan.nextDouble();
+			System.out.println("How important is the quality of the TAs?");
+			double taQuality = scan.nextDouble();
+			System.out.println("How important is it that others would recommend the course for Computer Science majors?");
+		    double recommendMajor = scan.nextDouble();
+			System.out.println("How important is it that others would recommend the course for non-Computer Science majors?");
+		    double recommendNonMajor = scan.nextDouble();
+			
+			DataModelCreator creator = new DataModelCreator("blank_text.txt", courseQuality, instructorQuality, difficulty, amountLearned, workRequired, 
+					readingsValue, communication, instructorAccess, stimulateInterest, taQuality, recommendMajor, recommendNonMajor);
+			creator.createDataFile(h);
 
 		}
 		catch (Exception e) {
